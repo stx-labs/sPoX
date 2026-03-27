629eca4d74606b4a94190ecdd2072a9685a8f9c7
Matteo Almanza <matteo.almanza@algorand.com>
2026-03-27 16:59:40 +0100

    feat: add solo stacker test instructions

---
+++ tests/solo/populate_env.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+spox_signers_xonly=$(
+  RUST_LOG=info cargo run -- -c tests/solo/spox.toml get-signers-xonly-key
+)
+
+export SPOX_DEPOSIT__ALICE__SIGNERS_XONLY="$spox_signers_xonly"
+export SPOX_DEPOSIT__BOB__SIGNERS_XONLY="$spox_signers_xonly"
+
+spox_deposit_addresses=$(
+  cargo run -- -c tests/solo/spox.toml get-deposit-address -n regtest
+)
+# This will output something like:
+#   alice: bcrt1psg8wccjeu2x4kalmvuy596aja2e34cz98t6uanm7fspp5hf05xyq5q294l
+#   bob: bcrt1p9rm286h5q2dfr6230qesd34frku8teps9vv9szyye6l0j55xqxrqssaklm
+
+while IFS=: read -r name addr; do
+  name=$(printf '%s' "$name" | tr '[:lower:]' '[:upper:]')
+  addr=${addr# }
+  export "SPOX_DEMO_${name}_BTC_ADDRESS=$addr"
+done <<EOF
+$spox_deposit_addresses
+EOF
+
+export SPOX_DEMO_ALICE_SK=3b1faa3d852d63860867557a9cf0587a06e89b9d8a385bee7139c7e65be7382401
+export SPOX_DEMO_BOB_SK=a46142592eaa52d38d17e07ddcf756c68a3efdab6a400fee9bc079ae7bfb3bf601
+
+unset spox_signers_xonly spox_deposit_addresses
