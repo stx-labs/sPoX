3192b9543efbe248a376d228cfb4cc15bdd5df2d
Matteo Almanza <matteo@stackslabs.com>
2026-04-16 17:46:12 +0200

    feat: add registry config

--- src/config/error.rs
+++ src/config/error.rs
@@ -8,6 +8,14 @@ pub enum SpoxConfigError {
     #[error("duration for {0} must be nonzero")]
     ZeroDurationForbidden(&'static str),
 
+    /// Missing required Stacks config
+    #[error("missing required stacks config")]
+    MissingStacksConfig,
+
+    /// Registry contract address is not a contract address
+    #[error("registry contract address is not a contract address")]
+    RegistryIsStandard,
+
     /// An error returned during parsing and building the configuration object
     #[error("cannot parse and build configuration: {0}")]
     ConfigError(#[from] ConfigError),
